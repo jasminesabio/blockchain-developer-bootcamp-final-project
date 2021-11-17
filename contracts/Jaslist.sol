dede22dec4dbf41d8f3d3d9c87c7ff9a0f8e4de3
Sabio <jasmine.sabio@gmail.com>
2021-11-16 22:50:17 -0500

    refactored smart contract and built out tests

--- contracts/Jaslist.sol
+++ contracts/Jaslist.sol
@@ -11,7 +11,7 @@ contract Jaslist {
     // address public admin;
 
     address owner;
-    uint public sku; 
+    uint public itemCount; 
 
     mapping (uint => Item) public items;
     mapping (uint => address) itemToOwner; //maps item to owner
@@ -29,119 +29,138 @@ contract Jaslist {
         string description;
         uint sku;
         uint price;
-        State state;
-        address payable buyer;
-        address payable seller;
-        address owner;
+        address payable owner;
+        bool purchased;
     }
     
     // User[] users;
     // Item[] items; // dynamic Array of Item struct ((Item[] public items;))// getter method is automatically created; other contracts can read from this array
 
-    enum State {
-        ForSale,
-        Sold,
-        Transferred,
-        Received,
-        NotForSale
-    }
+    // enum State {
+    //     ForSale,
+    //     Sold,
+    //     Transferred,
+    //     Received,
+    //     NotForSale
+    // }
 
-    State public state;
+    // State public state;
     
     constructor() public {
         owner = msg.sender;
-        sku = 0;
+        itemCount = 0;
     }
 
-    event LogItemAdded(uint sku);
-    event LogItemSold(uint sku);
-    event LogItemTransferred(uint sku);
-    event LogItemReceived(uint sku);
+    event LogItemAdded(
+        string name,
+        string description,
+        uint sku,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
 
-    modifier isOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-    
-    modifier forSale(uint _sku) {
-        require(items[_sku].seller != address(0));
-        require(items[_sku].state == State.ForSale, "Item is not for sale.");
-        _;
-    }
+    event LogItemSold(
+        string name,
+        string description,
+        uint sku,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
 
-    modifier sold(uint _sku) {
-        require(items[_sku].state == State.Sold, "Item has not been sold.");
-        _;
-    }
+    // event LogItemTransferred(uint sku);
+    // event LogItemReceived(uint sku);
 
-    modifier transferred(uint _sku) {
-        require(items[_sku].state == State.Transferred, "Item has not yet been transferred.");
-        _;
-    }
+    // modifier isOwner() {
+    //     require(msg.sender == owner);
+    //     _;
+    // }
 
     modifier paidEnough(uint _price) {
         require(msg.value >= _price);
         _;
     }
-
-    modifier checkValue(uint _sku) {
-        _;
-        uint _price = items[_sku].price;
-        uint amountToRefund = msg.value - _price;
-        items[_sku].buyer.transfer(amountToRefund);
-    }
-
-    modifier verifyCaller(address _address) {
-        require(msg.sender == _address);
-        _;
-    }
     
-    // func addUser(address _userAddress) {
-    //     msg.sender = _userAddress;
-    //     users[_userAddress] = User(_userAddress, false, true);
+    // modifier forSale(uint _sku) {
+    //     require(items[_sku].owner != address(0));
+    //     require(items[_sku].state == State.ForSale, "Item is not for sale.");
+    //     _;
     // }
-    
-    // func isSeller {
-    //     // checks to see if user can receive payment
+
+    // modifier sold(uint _sku) {
+    //     require(items[_sku].state == State.Sold, "Item has not been sold.");
+    //     _;
+    // }
+
+    // modifier transferred(uint _sku) {
+    //     require(items[_sku].state == State.Transferred, "Item has not yet been transferred.");
+    //     _;
+    // }
+
+    // modifier checkValue(uint _sku) {
+    //     _;
+    //     uint _price = items[_sku].price;
+    //     uint amountToRefund = msg.value - _price;
+    //     items[_sku].buyer.transfer(amountToRefund);
+    // }
+
+    // modifier verifyCaller(address _address) {
+    //     require(msg.sender == _address);
+    //     _;
     // }
 
     // why does function need to return true?
     function addItem(string memory _name, string memory _description, uint _price) public returns (bool) {
-        items[sku] = Item({
+
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        items[itemCount] = Item({
             name: _name,
             description: _description,
-            sku: sku,
+            sku: itemCount,
             price: _price,
-            state: State.ForSale,
-            buyer: address(0),
-            seller: msg.sender,
-            owner: msg.sender
+            owner: msg.sender,
+            purchased: false
         });
 
-        sku++;
-        emit LogItemAdded(sku);
+        itemCount++;
+        
+        emit LogItemAdded(_name, _description, items[itemCount].sku, _price, items[itemCount].owner, items[itemCount].purchased);
+        
         return true;
     }
     
-    function buyItem(uint _sku) public payable forSale(_sku) paidEnough(items[_sku].price) checkValue(_sku) {
-        items[_sku].buyer = msg.sender;
-        items[_sku].state = State.Sold;
-        items[_sku].seller.transfer(items[_sku].price);
+    
+    // test this function; dapp university 1:08:54
+    function buyItem(uint _sku) public payable paidEnough(items[_sku].price) {
         
-        emit LogItemSold(_sku);
+        address payable _seller = items[_sku].owner;
+        
+        require(_sku >= 0 && _sku <= itemCount);
+        require(items[_sku].purchased == false);
+        require(_seller != msg.sender);
+        
+        _seller.transfer(items[_sku].price);
+
+        items[_sku].owner = msg.sender;
+        items[_sku].purchased = true;
+        
+        emit LogItemSold(items[_sku].name, items[_sku].description, items[_sku].sku, items[_sku].price, items[_sku].owner, items[_sku].purchased);
     }
 
-    function transferItem(uint _sku) public sold(sku) verifyCaller(items[_sku].seller) {
-        items[_sku].state = State.Transferred;
+    // function transferItem(uint _sku) public sold(_sku) {
+    //     items[_sku].state = State.Transferred;
 
-        emit LogItemTransferred(_sku);
-    }
+    //     emit LogItemTransferred(_sku);
+    // }
 
-    function receiveItem(uint _sku) public transferred(_sku) verifyCaller(items[_sku].buyer) {
-        items[_sku].state = State.Received;
+    // function receiveItem(uint _sku) public transferred(_sku) {
+    //     items[_sku].state = State.Received;
 
-        emit LogItemReceived(_sku);
-    }
+    //     emit LogItemReceived(_sku);
+    // }
 
     function fetchItemName(uint _sku) public view returns (string memory name) {
         name = items[_sku].name;
@@ -155,25 +174,13 @@ contract Jaslist {
         return (name);
     }
 
-    function fetchItemPrice(uint _sku) public view returns (uint price) {
-        // name = items[_sku].name;
-        // description = items[_sku].description;
-        // sku = items[_sku].sku;
-        price = items[_sku].price;
-        // state = uint(items[_sku].state);
-        // buyer = items[_sku].buyer;
-        // seller = items[_sku].seller;
-        
-        return (price);
-    }
+    // function updateItemPrice(uint _sku, uint _price) public isOwner {
+    //     items[_sku].price = _price;
+    // }
 
-    function updateItemPrice(uint _sku, uint _price) public isOwner {
-        items[_sku].price = _price;
-    }
-
-    function itemNotForSale(uint _sku) public {
-        items[_sku].state = State.NotForSale;
-    }
+    // function itemNotForSale(uint _sku) public {
+    //     items[_sku].state = State.NotForSale;
+    // }
 
     // function getItemsByOwner {
 
--- test/jaslist.test.js
+++ test/jaslist.test.js
@@ -1,34 +1,86 @@
-const { assert } = require("console");
-
 const Jaslist = artifacts.require("Jaslist");
 
-contract('Jaslist', (accounts) => {
+// require('chai')
+//     .use(require('chai-as-promised'))
+//     .should()
 
-    let jaslist = null;
-    const [seller, buyer] = accounts;
+contract('Jaslist', ([deployer, seller, buyer]) => {
+
+    let jaslist;
 
     before(async() => {
         jaslist = await Jaslist.deployed();
     });
 
     describe('deployment', async() => {
-        it('should deploy smart contract properly', async () => {
+        it('should deploy smart contract properly', async() => {
             // console.log(jaslist.address);
             assert(jaslist.address !== '');
         });
+
+        it('has an item count', async() => {
+            const itemCount = await jaslist.itemCount();
+            assert.equal(itemCount, 0);
+        })
     });
 
     describe('items', async() => {
-        it('increases product count', async() => {
-            const itemCount = await jaslist.sku();
-            await jaslist.addItem("test", "test description", web3.utils.toWei('1', 'Ether'));
-            assert(itemCount == 1);
+        let itemAdded;
+        let itemCount;
+
+        before(async() => {
+            itemAdded = await jaslist.addItem("test", "test description", web3.utils.toWei('1', 'Ether'), { from: seller });
+            itemCount = await jaslist.itemCount();
         });
 
         it('should add item', async() => {
-            await jaslist.addItem("test", "test description", web3.utils.toWei('1', 'Ether'));
-            const item = await jaslist.fetchItemName(0); 
-            assert(item, "test");
+            const itemAddedData = itemAdded.logs[0].args;
+            console.log(itemAddedData);
+            assert.equal(itemAddedData.sku.toNumber(), itemCount.toNumber()-1, "sku is not correct")
+            // assert.equal(itemAddedData.name, "test", "name is not correct");
+            assert.equal(itemAddedData.owner, seller, "owner is not correct");
+
+            //Failure:
+            // await jaslist.addItem("", "test description", web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+        });
+
+        it('increases item count', async() => {
+            assert.equal(itemCount, 1);
+        });
+
+        it('lists items', async() => {
+            // const item = await jaslist.fetchItemName(0); 
+            // assert(item, "test");
+
+            const item = await jaslist.items(itemCount-1);
+            assert.equal(item.name, "test", "name is not correct")
+        });
+
+        it('sells items and transfers eth to seller', async() => {
+            let oldSellerBalance;
+            oldSellerBalance = await web3.eth.getBalance(seller);
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance);
+            
+            purchase = await jaslist.buyItem(0, { from: buyer, value: web3.utils.toWei('1', 'Ether') });
+            
+            const itemSoldData = purchase.logs[0].args;
+            console.log(itemSoldData);
+            assert.equal(itemSoldData.name, "test", "name is not correct");
+            assert.equal(itemSoldData.owner, buyer, "owner is not correct");
+
+            let newSellerBalance;
+            newSellerBalance = await web3.eth.getBalance(seller);
+            newSellerBalance = new web3.utils.BN(newSellerBalance);
+
+            let price;
+            price = web3.utils.toWei('1', 'Ether');
+            price = new web3.utils.BN(price);
+
+            console.log(oldSellerBalance.toString(), newSellerBalance.toString(), price.toString());
+
+            const expectedBalance = oldSellerBalance.add(price);
+            assert.equal(newSellerBalance.toString(), expectedBalance.toString());
+
         });
 
     });

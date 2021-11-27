9513f27114fbc999188b72563f8abbd5ac1a5a29
Sabio <jasmine.sabio@gmail.com>
2021-11-27 14:19:43 -0500

    udpated tests

--- test/jaslist.test.js
+++ test/jaslist.test.js
@@ -1,10 +1,5 @@
 const { assert } = require("chai");
-
 const Jaslist = artifacts.require("Jaslist");
-
-// require('chai')
-//     .use(require('chai-as-promised'))
-//     .should()
 
 contract('Jaslist', ([deployer, seller, buyer]) => {
 
@@ -16,7 +11,6 @@ contract('Jaslist', ([deployer, seller, buyer]) => {
 
     describe('deployment', async() => {
         it('should deploy smart contract properly', async() => {
-            // console.log(jaslist.address);
             assert(jaslist.address !== '');
         });
 
@@ -37,18 +31,11 @@ contract('Jaslist', ([deployer, seller, buyer]) => {
 
         it('should add item and increase item count', async() => {
             const itemAddedData = itemAdded.logs[0].args;
-            // console.log("sku number is", itemAddedData.sku.toNumber());
-            // console.log("item count is:", itemCount.toNumber(0))
-            // // assert.equal(itemCount.toNumber(), 1);
-            // assert.equal(itemAddedData.sku.toNumber(), itemCount.toNumber(), "sku is not correct")
-            assert.equal(itemAddedData.name, "test", "name is not correct");
-            assert.equal(itemAddedData.itemOwner, seller, "item owner is not correct");
+            assert.equal(itemAddedData._name, "test", "name is not correct");
+            assert.equal(itemAddedData._itemOwner, seller, "item owner is not correct");
         });
 
         it('lists items', async() => {
-            // const item = await jaslist.fetchItemName(0); 
-            // assert(item, "test");
-            // console.log(itemCount);
             const item = await jaslist.items(1);
             assert.equal(item.name, "test", "name is not correct")
             assert.equal(item.itemOwner, seller, "item owner is not correct")
@@ -56,33 +43,29 @@ contract('Jaslist', ([deployer, seller, buyer]) => {
         });
 
         it('sells items and transfers eth to seller', async() => {
-        //     let oldSellerBalance;
-        //     oldSellerBalance = await web3.eth.getBalance(seller);
-        //     oldSellerBalance = new web3.utils.BN(oldSellerBalance);
+            let oldSellerBalance;
+            oldSellerBalance = await web3.eth.getBalance(seller);
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance);
             
-        //     purchase = await jaslist.buyItem(1, { from: buyer, value: web3.utils.toWei('1', 'Ether') });
+            purchase = await jaslist.buyItem(1, { from: buyer, value: web3.utils.toWei('1', 'Ether') });
             
-        //     const itemSoldData = purchase.logs[0].args;
-        //     // console.log(itemSoldData);
-        //     assert.equal(itemSoldData.name, "test", "name is not correct");
-        //     assert.equal(itemSoldData.itemOwner, buyer, "item owner is not correct");
+            const itemSoldData = purchase.logs[0].args;
+            // console.log(itemSoldData);
+            assert.equal(itemSoldData._name, "test", "name is not correct");
+            assert.equal(itemSoldData._itemOwner, buyer, "item owner is not correct");
 
-        //     let newSellerBalance;
-        //     newSellerBalance = await web3.eth.getBalance(seller);
-        //     newSellerBalance = new web3.utils.BN(newSellerBalance);
+            let newSellerBalance;
+            newSellerBalance = await web3.eth.getBalance(seller);
+            newSellerBalance = new web3.utils.BN(newSellerBalance);
 
-        //     let price;
-        //     price = web3.utils.toWei('1', 'Ether');
-        //     price = new web3.utils.BN(price);
+            let price;
+            price = web3.utils.toWei('1', 'Ether');
+            price = new web3.utils.BN(price);
 
-        //     // console.log(oldSellerBalance.toString(), newSellerBalance.toString(), price.toString());
+            // console.log(oldSellerBalance.toString(), newSellerBalance.toString(), price.toString());
 
-        //     const expectedBalance = oldSellerBalance.add(price);
-        //     assert.equal(newSellerBalance.toString(), expectedBalance.toString());
-
-        // });
-
-        
+            const expectedBalance = oldSellerBalance.add(price);
+            assert.equal(newSellerBalance.toString(), expectedBalance.toString());
 
         });
 
@@ -90,24 +73,9 @@ contract('Jaslist', ([deployer, seller, buyer]) => {
             itemAddedToBeDeleted = await jaslist.addItem("test2", "test description2", web3.utils.toWei('1', 'Ether'), { from: seller });
             const removesItem = await jaslist.removeItem(2, { from: seller });
             const item = await jaslist.items(2);
-            // assert.equal(item.itemOwner, seller, "item owner is not correct")
-            // console.log("seller:", seller);
-            // console.log(item);
             assert.equal(item.name, "ticket unavailable", "item has not been removed")
         });
 
     });
 
-
-    
-
-    
-    
-    // it('should update price', async() => {
-    //     await jaslist.addItem("test1", "test description1", 1, { from: seller });
-    //     await jaslist.updateItemPrice(0, 2, { from: seller });
-    //     const item = await jaslist.fetchItemPrice(0);
-    //     console.log("this is item:", item);
-    //     assert.equal(item, 2, "Error message.");
-    // });
 });
